9c2941836780e2bb5631fb61b70cd9b2d7b55855
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-06 14:53:16 -0300

    Props and lib Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard/Index";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
 export function App() {
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] =
+    useState(false);
+
+  const handleOpenTrasactionModal = () => {
+    setIsNewTrasactionModalOpen(true);
+  };
+  const handleCloseTrasactionModal = () => {
+    setIsNewTrasactionModalOpen(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header handleOpenTrasactionModal={handleOpenTrasactionModal} />
       <GlobalStyle />
+      <Modal
+        isOpen={isNewTrasactionModalOpen}
+        onRequestClose={handleCloseTrasactionModal}
+      >
+        <h2>cadastrar transação</h2>
+      </Modal>
       <Dashboard />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import LogoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styled";
 
-export function Header() {
+interface HeaderProps {
+  handleOpenTrasactionModal: () => void;
+}
+export function Header({ handleOpenTrasactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={LogoImg} alt="Dt Money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenTrasactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
