7437697b4309632ae2e77459673ce310eb2ef56a
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-08 20:24:13 -0300

    insert buttons custom form

--- src/components/NewTrasactionModal/index.tsx
+++ src/components/NewTrasactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import incomeImg from "../../assets/income.svg";
 
 import * as S from "./styled";
 
@@ -33,6 +35,16 @@ export function NewTrasactionModal({
 
         <input type="text" placeholder="titulo" />
         <input type="number" placeholder="valor" />
+        <S.TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </S.TransactionTypeContainer>
         <input type="text" placeholder="categoria" />
         <button type="submit">Cadastrar</button>
       </S.Container>
