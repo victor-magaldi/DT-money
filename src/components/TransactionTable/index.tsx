93f03ac0ff06e7c5e6f37ee355ed501f8071391c
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-08 22:01:04 -0300

    Listando transactions

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,10 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import * as S from "./styled";
 
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  category: string;
+  amount: number;
+  createdAt: Date;
+}
 export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   useEffect(() => {
-    api.get("/transactions").then((r) => console.log(r?.data));
+    api.get("/transactions").then((r) => setTransactions(r.data.transactions));
   }, []);
 
   return (
@@ -19,18 +28,16 @@ export function TransactionTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="title">desenvolvimento web site</td>
-            <td className="deposit">R$ 12000,00</td>
-            <td>desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td className="title">Aluguel</td>
-            <td className="withdraw">R$ -800,00</td>
-            <td>casa</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions.map((transaction) => {
+            return (
+              <tr key={transaction.id}>
+                <td className={transaction.title}>{transaction.title}</td>
+                <td className="deposit">R$ {String(transaction.amount)}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </S.Container>
--- src/mock/mirageMock.ts
+++ src/mock/mirageMock.ts
@@ -1,11 +1,36 @@
 import { createServer, Model } from "miragejs";
 
+
 export function startMock() {
 
     createServer({
         //model define um mini banco de dados do mirage
         models: {
             transaction: Model
+        },
+        seeds(server) {
+            server.db.loadData({
+                transactions: [
+                    {
+                        id: 1,
+                        title: "freelance website",
+                        type: "deposit",
+                        amount: 6000,
+                        category: "desenvolvimento",
+                        createdAt: new Date('2021-03-10 08:00:00')
+                    },
+                    {
+                        id: 2,
+                        title: "aluguel",
+                        type: "withdraw",
+                        amount: 1100,
+                        category: "casa",
+                        createdAt: new Date('2021-03-10 08:00:00')
+                    },
+
+                ]
+            }
+            )
         },
         routes() {
             this.namespace = "api";
